4bbe59ddda06952799d0da5ac83a1e40994ed323
merinzacharias <merzak2601@gmail.com>
2025-01-30 12:09:34 +0530

    Added array for service cards

--- src/pages/routingPages/About.jsx
+++ src/pages/routingPages/About.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import store from "../../assets/restaurant_store.jpg"
-import spoon from "../../assets/spoon_fork.png"
 import cook from "../../assets/cook.png"
 import plate from "../../assets/plate.png"
+import store from "../../assets/restaurant_store.jpg"
 
 const Services_cards = [
   {
     id: 1,
-    image: pasta,
+    image: cook,
     title: "Best Chef",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
 
@@ -15,15 +14,14 @@ const Services_cards = [
   },
   {
     id: 2,
-    image: pasta,
-    title: "Best Chef",
+    image: plate,
+    title: "Quality Food",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
   },
   {
     id: 3,
-    name: "Tomato Salad",
-    image: pasta,
-    title: "Best Chef",
+    image: cook,
+    title: "Perfect Cook",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
   },  
 ];
@@ -63,18 +61,20 @@ const About = () => {
         <h3 className='text-[#FB8A22] text-[18px] Inter-SemiBold'>Services We Offer</h3>
         <div className='flex justify-between items-center mx-30'>
     
-    
-          <div className='flex flex-col justify-center gap-3 items-center w-[100%] bg-white shadow-2xl lg:w-[25%] py-15 px-8'>
+        {Services_cards.map((item) => (
+          <div key={item.id} className='flex flex-col justify-center gap-3 items-center w-[100%] bg-white shadow-2xl lg:w-[25%] py-15 px-8'>
             <div className='w-20 h-20 md:w-30 md:h-30 rounded-full bg-[#FAE8DC] flex justify-center items-center '>
-              <img src={cook} alt='badge-icon' className='h-15 md:h-15 ' />
+              <img src={item.image} alt='badge-icon' className='h-15 md:h-15 ' />
             </div>
             <div className='flex flex-col justify-between items-center'>
-              <h3 className='pt-5 Inter-Medium text-[18px] md:text-[18px] lg:text-[20px]'>Best Chef</h3>
-              <p className='Inter-Regular text-[16px] md:text-[13px] lg:text-[15px] text-[#646573] pt-5 text-center px-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
+              <h3 className='pt-5 Inter-Medium text-[18px] md:text-[18px] lg:text-[20px]'>{item.title}</h3>
+              <p className='Inter-Regular text-[16px] md:text-[13px] lg:text-[15px] text-[#646573] pt-5 text-center px-8'>{item.description}</p>
             </div>
           </div>
+                    ))}
 
-          {/* <div className='flex flex-col justify-center gap-5 items-center w-[100%] bg-white shadow-2xl lg:w-[25%] py-15 px-8'>
+
+           {/* <div className='flex flex-col justify-center gap-5 items-center w-[100%] bg-white shadow-2xl lg:w-[25%] py-15 px-8'>
             <div className='w-20 h-20 md:w-30 md:h-30 rounded-full bg-[#FAE8DC] flex justify-center items-center '>
               <img src={plate} alt='badge-icon' className='h-15 md:h-15 ' />
             </div>
@@ -92,7 +92,7 @@ const About = () => {
               <h3 className='pt-5 Inter-Medium text-[18px] md:text-[18px] lg:text-[20px]'>Perfect Cook</h3>
               <p className='Inter-Regular text-[16px] md:text-[13px] lg:text-[15px] text-[#646573] pt-5 text-center px-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
             </div>
-          </div>    */}
+          </div>     */}
 
 
 
